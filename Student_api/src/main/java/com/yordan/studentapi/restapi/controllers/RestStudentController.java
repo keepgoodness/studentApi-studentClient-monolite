9898ec512e1d342fdc1e15714e52e0eed21ce24e
keepgoodness <yordanlazarov83@gmail.com>
2018-12-20 17:16:22 +0200

    make post form for insert student - unfinished

--- Student_api/src/main/java/com/yordan/studentapi/restapi/controllers/RestStudentController.java
+++ Student_api/src/main/java/com/yordan/studentapi/restapi/controllers/RestStudentController.java
@@ -7,6 +7,8 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,11 +33,17 @@ public class RestStudentController {
 	}
 
 	@GetMapping("/{id}")
-	public Student findById(@PathVariable String id) {
+	public Student findById(@PathVariable(name = "id") String id) {
 		Optional<Student> findById = sr.findById(Long.parseLong(id));
 		if (findById.isPresent()) {
 			return findById.get();
 		}
 		return null;
+	}
+
+	@PostMapping("/student")
+	public Student saveStudent(@RequestBody Student student) {
+
+		return sr.save(student);
 	}
 }
